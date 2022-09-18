4d3398c7f8192158119a3178ff165d3f8b56df04
Enes Tunahan Çömez <enesstunahan@gmail.com>
2022-09-18 21:59:29 +0300

    Controller yazıldı

---
+++ Covid19SignalR.API/Controllers/CovidsController.cs
@@ -0,0 +1,46 @@
+﻿using Covid19SignalR.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Covid19SignalR.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CovidsController : ControllerBase
+    {
+        private readonly CovidService _covidService;
+
+        public CovidsController(CovidService covidService)
+        {
+            _covidService = covidService;   
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SaveCovid(Covid covid)
+        {
+            await _covidService.SaveCovid(covid);
+            IQueryable<Covid> covidList = _covidService.GetList();
+            return Ok(covidList);
+        }
+
+        [HttpGet]
+        public  IActionResult InitializeCovid()
+        {
+
+            Random random = new Random();
+            Enumerable.Range(1, 10).ToList().ForEach(x =>
+            {
+               
+                foreach(ECity item in Enum.GetValues(typeof(ECity)))
+                {
+                    var newCovid = new Covid { City = item, Count = random.Next(100, 1000), CovidDate = DateTime.Now.AddDays(x) };
+                    _covidService.SaveCovid(newCovid).Wait();  // işlemi burada blokla devam etmesine izin verme demek
+                    Thread.Sleep(1000);
+                }
+            
+            });
+
+            return Ok("Datalar başarıyla db ye kaydedildi");
+        }
+    }
+}
